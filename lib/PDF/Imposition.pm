b19823139f0f8ae5c1ed10ffde845860330dbee7
Marco <melmothx@gmail.com>
2013-06-03 23:09:43 +0200

    Fixed doc

--- lib/PDF/Imposition.pm
+++ lib/PDF/Imposition.pm
@@ -13,7 +13,7 @@ use File::Copy;
 
 =head1 NAME
 
-PDF::Imposition - Perl module to manage the PDF imposition.
+PDF::Imposition - Perl module to manage the PDF imposition
 
 =head1 VERSION
 
@@ -26,11 +26,15 @@ our $VERSION = '0.01';
 
 =head1 SYNOPSIS
 
+This module is meant to simplify the so-called imposition, i.e.,
+rearrange the pages of a PDF to get it ready to be printed and folded,
+with more logical pages placed on the sheet, on recto and verso.
+
     use PDF::Imposition;
     my $imposer = PDF::Imposition->new(file => "test.pdf", cover => 1);
     $imposer->signature(50); # or $imposer->signature("50-80");
     $imposer->impose;
-
+    print "Output left in " . $imposer->outfile;
 
 =head1 SUBROUTINES/METHODS
 
@@ -440,12 +444,9 @@ Marco Pessotto, C<< <melmothx at gmail.com> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-pdf-imposition at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=PDF-Imposition>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-
-
+Please report any bugs or feature requests to the author's email. If
+you find a bug, please provide a minimal muse file which reproduces
+the problem (so I can add it to the test suite).
 
 =head1 SUPPORT
 
@@ -453,73 +454,32 @@ You can find documentation for this module with the perldoc command.
 
     perldoc PDF::Imposition
 
-
-You can also look for information at:
+=head1 SEE ALSO
 
 =over 4
 
-=item * RT: CPAN's request tracker (report bugs here)
+=item psutils
 
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=PDF-Imposition>
+L<http://knackered.org/angus/psutils/> (shipped by any decent
+GNU/Linux distro and in TeXlive!). If you don't bother the
+PDF->PS->PDF route, it's a great and useful tool which just aged well.
 
-=item * AnnoCPAN: Annotated CPAN documentation
+=item pdfpages
 
-L<http://annocpan.org/dist/PDF-Imposition>
+L<http://www.ctan.org/pkg/pdfpages>
 
-=item * CPAN Ratings
+=item pdfjam
 
-L<http://cpanratings.perl.org/d/PDF-Imposition>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/PDF-Imposition/>
+L<http://www2.warwick.ac.uk/fac/sci/statistics/staff/academic-research/firth/software/pdfjam/>
+(buil on the top of psutils)
 
 =back
 
+=head1 LICENSE
 
-=head1 ACKNOWLEDGEMENTS
-
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2013 Marco Pessotto.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of the the Artistic License (2.0). You may obtain a
-copy of the full license at:
-
-L<http://www.perlfoundation.org/artistic_license_2_0>
-
-Any use, modification, and distribution of the Standard or Modified
-Versions is governed by this Artistic License. By using, modifying or
-distributing the Package, you accept this license. Do not use, modify,
-or distribute the Package, if you do not accept this license.
-
-If your Modified Version has been derived from a Modified Version made
-by someone other than you, you are nevertheless required to ensure that
-your Modified Version complies with the requirements of this license.
-
-This license does not grant you the right to use any trademark, service
-mark, tradename, or logo of the Copyright Holder.
-
-This license includes the non-exclusive, worldwide, free-of-charge
-patent license to make, have made, use, offer to sell, sell, import and
-otherwise transfer the Package with respect to any patent claims
-licensable by the Copyright Holder that are necessarily infringed by the
-Package. If you institute patent litigation (including a cross-claim or
-counterclaim) against any party alleging that the Package constitutes
-direct or contributory patent infringement, then this Artistic License
-to you shall terminate on the date that such litigation is filed.
-
-Disclaimer of Warranty: THE PACKAGE IS PROVIDED BY THE COPYRIGHT HOLDER
-AND CONTRIBUTORS "AS IS' AND WITHOUT ANY EXPRESS OR IMPLIED WARRANTIES.
-THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
-PURPOSE, OR NON-INFRINGEMENT ARE DISCLAIMED TO THE EXTENT PERMITTED BY
-YOUR LOCAL LAW. UNLESS REQUIRED BY LAW, NO COPYRIGHT HOLDER OR
-CONTRIBUTOR WILL BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, OR
-CONSEQUENTIAL DAMAGES ARISING IN ANY WAY OUT OF THE USE OF THE PACKAGE,
-EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
+This program is free software; you can redistribute it and/or modify
+it under the terms of either: the GNU General Public License as
+published by the Free Software Foundation; or the Artistic License.
 
 =cut
 
