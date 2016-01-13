7e765ecb83785cd548e3100154060b4317bd3b7b
Marco <melmothx@gmail.com>
2016-01-13 14:27:22 +0100

    Changes and version bump

--- lib/PDF/Imposition.pm
+++ lib/PDF/Imposition.pm
@@ -23,11 +23,11 @@ PDF::Imposition - Perl module to manage the PDF imposition
 
 =head1 VERSION
 
-Version 0.20
+Version 0.21
 
 =cut
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 
 =head1 SYNOPSIS
